ab50d73b3b4c6b396e3b7d5188f9de71148f311f
Akshey Saluja <akshay181@gmail.com>
2019-02-04 21:25:29 -0500

    updated DoorLock

--- doorlock.js
+++ doorlock.js
@@ -109,3 +109,8 @@ function unlockDoor() {
   	//After 1.5 seconds, the door lock servo turns off to avoid stall current
   	setTimeout(function(){motor.servoWrite(0)}, 1500)
 }
+
+function videoStream(){
+
+  
+}
